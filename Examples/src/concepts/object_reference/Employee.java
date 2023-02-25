cd0954982fa9e087ffe57fedb3038198b023ddb5
sumit Rana <sumit2628r@gmail.com>
2023-02-25 23:33:17 +0530

    employee updated

--- Examples/src/concepts/object_reference/Employee.java
+++ Examples/src/concepts/object_reference/Employee.java
@@ -1,4 +1,22 @@
 package concepts.object_reference;
 
 public class Employee {
+    private int employeeId;
+    private String employeeName;
+
+    public int getEmployeeId() {
+        return employeeId;
+    }
+
+    public void setEmployeeId(int employeeId) {
+        this.employeeId = employeeId;
+    }
+
+    public String getEmployeeName() {
+        return employeeName;
+    }
+
+    public void setEmployeeName(String employeeName) {
+        this.employeeName = employeeName;
+    }
 }
