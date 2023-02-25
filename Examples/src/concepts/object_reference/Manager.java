7a6891ff570db892479c55f5ce3d94a60293094a
sumit Rana <sumit2628r@gmail.com>
2023-02-25 23:33:39 +0530

    manager updated

--- Examples/src/concepts/object_reference/Manager.java
+++ Examples/src/concepts/object_reference/Manager.java
@@ -1,4 +1,28 @@
 package concepts.object_reference;
 
+import java.util.PrimitiveIterator;
+
 public class Manager {
+    private int managerId;
+    private String managerName;
+
+   public Manager(Employee e){
+       managerId = e.getEmployeeId();
+       managerName = e.getEmployeeName();
+
+   }
+
+   public void managerData(){
+       System.out.println("Manager Id: "+ managerId);
+       System.out.println("Manager name:"+ managerName);
+
+   }
+
+    @Override
+    public String toString() {
+        return "Manager{" +
+                "managerId=" + managerId +
+                ", managerName='" + managerName + '\'' +
+                '}';
+    }
 }
